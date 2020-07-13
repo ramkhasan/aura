b29b46228c8f9edbf933fbb093cc8a4c62e5d95e
unknown <ramhas87@gmail.com>
2020-07-13 17:34:22 +0300

    исправил отображение логотипа в подвале сайта: загрузил logo.svg в папку contacts с точными размерами, заменил путь к логотипу в index.html, изменил стили в подвале; добавил недостающий шрифт useoansyrl-700.eot; заменил border при фокусе кнопок на box-shadow; не до конца сверстал мобильное меню-бургер; изменил размеры svg-иконок в инпутах форм авторизации/регистрации

--- src/js/main.js
+++ src/js/main.js
@@ -1,13 +1,3 @@
-// Плавная прокрутка до якоря меню
-$(document).ready(function () {
-  $("#menu").on("click", "a", function (event) {
-    event.preventDefault();
-    var id = $(this).attr("href"),
-      top = $(id).offset().top;
-    $("body,html").animate({ scrollTop: top }, 1500);
-  });
-});
-
 // Показать/скрыть карточки товаров
 var areCardsShown = false;
 $(".btn-toggle-cards").on("click", function () {
@@ -20,33 +10,31 @@ $(".btn-toggle-cards").on("click", function () {
     $(".flip-container.card-toggle").addClass("show");
     $(this).text("Скрыть");
   }
-
   areCardsShown = !areCardsShown;
 });
 
-// Гамбургер-меню
-// Трансформируем 4 полоски в крестик
-const elem = document.getElementById("hamburger");
-
-const toggle = (e) => {
-  const className = ` ${hamburger.className} `;
-  if (className.includes(" open ")) {
-    e.currentTarget.className = className.replace(" open ", " ");
-  } else {
-    e.currentTarget.className += " open";
-  }
-};
-
-elem.addEventListener("click", toggle);
-
-// Открываем и закрываем меню при клике на гамбургер
-const menu = document.querySelector(".header-mob__menu");
-elem.addEventListener("click", function () {
-  menu.classList.toggle("active");
+//Меню на мобильных устройствах
+// полноэкранное мобильное меню
+$(".nav__trigger").on("click", function (e) {
+  e.preventDefault(); // предотвращаем поведение по умолчанию
+  $(this).parent().toggleClass("nav--active").show(); //добавляем класс nav--active к nav icon
+  $(".nav__trigger").toggleClass("nav__trigger--active"); //добавляем класс nav__trigger--active к nav nav__trigger
+  $("body").toggleClass("no-scroll"); //отключаем прокрутку за навигацией
 });
-// Открываем и закрываем меню при клике на пункт меню
-$(".header-mob__menu-link").on("click", function () {
-  menu.classList.toggle("active");
+// нажатие на ссылку закрывает меню и включает прокрутку тела
+$(".nav li").on("click", function () {
+  $(".nav__trigger").parent().removeClass("nav--active");
+  $(".nav__icon").removeClass("nav--active");
+  $("body").removeClass("no-scroll");
+});
+// плавная прокрутка до якоря меню
+$(document).ready(function () {
+  $("#menu").on("click", "a", function (event) {
+    event.preventDefault();
+    var id = $(this).attr("href"),
+      top = $(id).offset().top;
+    $("body,html").animate({ scrollTop: top }, 1500);
+  });
 });
 
 // Кнопка плавной прокрутки вверх
@@ -58,7 +46,6 @@ $(document).ready(function () {
       $(".scrollup").fadeOut();
     }
   });
-
   $(".scrollup").click(function () {
     $("html, body").animate(
       {
